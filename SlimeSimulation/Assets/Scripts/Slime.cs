31af714aa6af3eb37e0971f2684ece0bab83d7ba
Bronson <bronson.schafer@gmail.com>
2022-10-05 14:38:59 -0600

    Boarders added
    object will now stay within the screen view.

--- SlimeSimulation/Assets/Scripts/Slime.cs
+++ SlimeSimulation/Assets/Scripts/Slime.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
-    float xDir= 1, yDir = 2;
-    float xSpeed = 1, ySpeed = 1;
+    float xDir= 1, yDir = 1;
+    float xSpeed = 0.05f, ySpeed = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(xDir*xSpeed, yDir*ySpeed, 0);
+        // make the pixel change direction after hitting the edge
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        // left 
+        if(pos.x < 0.0){
+            xDir = 1;
+        }
+        // right 
+        if(1.0 < pos.x){
+            xDir = -1;
+        }
+        // below
+        if(pos.y < 0.0){
+            yDir = 1;
+        }
+        // above 
+        if(1.0 < pos.y){
+            yDir = -1;
+        }
     }
 }
